3054d02f089d329d3bb4c7e803f77f63b0cdcb95
easonkuo <shuwei1106@gmail.com>
2021-01-20 16:12:56 +0800

    no message

--- chart_api/chart.py
+++ chart_api/chart.py
@@ -1,10 +1,9 @@
 import flask
 from flask import request,jsonify,make_response
 from flask_restful import Api,Resource
-# from resource1.chart1 import One
 import pymysql
 # from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 from flask import render_template
 import decimal
 import json
@@ -12,18 +11,7 @@ import json
 app = flask.Flask(__name__)
 app.config["DEBUG"]=True
 api =Api(app)
-# api.add_resource(All,'/All')
-# api.add_resource(One,'/One/1101')
-# api.add_resource(Select,'/select/Taiwan')
-# api.add_resource(Death,'/death/5000')
-# api.add_resource(User,'/user/<id>')
-# api.add_resource(Accounts,'/Accounts')
-# api.add_resource(Account,'/Account/<id>')
 
-
-@app.route('/hi' , methods = ['GET'])
-def hi():
-  return '<h3>hello!<h3>'
 
 @app.route("/debtratio/<code>", methods=["get"])
 def home(code):
@@ -33,60 +21,52 @@ def home(code):
 def eps(code):
     return render_template('eps.html',code_template = code)
 
-@app.route('/re/<code>', methods=["get"])
-def re(code):
-    return render_template('res.html',a = code)
+@app.route('/revenue/<code>', methods=["get"])
+def revenue(code):
+    return render_template('revenue.html',code_template = code)
 
-# def db_init():
-#     db = pymysql.connect('192.168.56.104','russell','pn12345','STOCK')
-#     cursor = db.cursor(pymysql.cursors.DictCursor)
-#     return db,cursor
+@app.route("/<code>", methods=["get"])
+def home1(code):
+    return render_template('index.html',code_template = code)
 
+# ========================== GET DATA ====================================
 
 @app.route('/getdebtratio/<code>', methods=["GET"])
 def debt_ratio(code):
     db, cursor = db_init()
 # json無法解析decimal格式，必須先將有小數的值轉為double
-    sql = """  Select concat(年度,'-',季度) as labels, convert(( 負債總計/資產總計)*100,double(10,2)) as value From STOCK.balancesheet915
+    sql = """  Select concat(年度,'-',季度) as labels, convert(( 負債總計/資產總計)*100,double(10,2)) as value From stock_database.balancesheet915
                     where code = '{}'
             """.format(code)
     cursor.execute(sql)
     one = cursor.fetchall()
     db.close()
-    # response={
-    #     'data':one
-    # }    
     return jsonify(one)
 
 def db_init():
-    db = pymysql.connect('192.168.56.104','russell','pn12345','STOCK')
+    db = pymysql.connect('192.168.56.102','eason','671106','stock_database')
     cursor = db.cursor(pymysql.cursors.DictCursor)
     return db,cursor
   
 @app.route('/geteps/<code>', methods=["GET"])
-
 def geteps(code):
     db, cursor = db_init()
 # json無法解析decimal格式，必須先將有小數的值轉為double
-    sql = """   SELECT 年度 as labels , 基本每股盈餘 as value FROM STOCK.incomestatement913 where code = '{}' and 季度 = 4 
+    sql = """   SELECT 年度 as labels , 基本每股盈餘 as value FROM stock_database.incomestatement913 where code = '{}' and 季度 = 4 
     or code = '{}' and 年度 = 109 and 季度= 3 order by 年度
             """.format(code,code)
     cursor.execute(sql)
     one = cursor.fetchall()
-    db.close()
-    # response={
-    #     'data':one
-    # }    
+    db.close() 
     return jsonify(one)
 
-@app.route('/get/<code>',methods=["GET"])
-
+@app.route('/getrevenue/<code>',methods=["GET"])
 def getrevenue(code):
     db, cursor = db_init()
     sql = """
     SELECT concat(t1.年度,'-',t1.季度) as labels,  	
 		IF (t1.季度=1, t1.營業收入,t1.營業收入-1- t2.營業收入) as value
-FROM STOCK.`8888` t1 left JOIN STOCK.`8888` t2 ON t1.id = t2.id+1  where t1.code='{}' order by t1.年度,t1.季度;
+    FROM stock_database.incomestatement913 t1 left JOIN stock_database.incomestatement913 t2 ON t1.id = t2.id+1  where t1.code='{}' order by t1.年度,t1.季度;
         """.format(code)
 
     cursor.execute(sql)
@@ -94,15 +74,19 @@ FROM STOCK.`8888` t1 left JOIN STOCK.`8888` t2 ON t1.id = t2.id+1  where t1.code
     db.close()
     return jsonify(one)
 
-# def db_init():
-#     db = pymysql.connect('192.168.56.104','russell','pn12345','STOCK')
-#     cursor = db.cursor(pymysql.cursors.DictCursor)
-#     return db,cursor
-# def db_init():
-#     db = pymysql.connect('192.168.56.104','russell','pn12345','STOCK')
-#     cursor = db.cursor(pymysql.cursors.DictCursor)
-#     return db,cursor
-    
+
+@app.route("/all/<code>", methods=["get"])
+def all(code) :
+    db, cursor = db_init()
+    sql = """
+        SELECT DATE_FORMAT(date,'%Y-%m-%d') date,open_price,close_price,low_price,high_price,FORMAT(stock_count,'no')
+        FROM stock_database.stock_date  where code = '{}';
+        """.format(code)
+
+    cursor.execute(sql)
+    count_data = cursor.fetchall()
+    db.close()
+    return jsonify(count_data)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',port=8000)
